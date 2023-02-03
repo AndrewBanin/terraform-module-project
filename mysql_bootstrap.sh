4284259640b53459c2ad1249e8ba833bf9616c29
banina <andybanin@gmail.com>
2023-02-02 21:30:35 -0500

    wordpress and mysql shell files modified

--- mysql_bootstrap.sh
+++ mysql_bootstrap.sh
@@ -2,21 +2,21 @@
 # Install MySQL
 # # Installation of mysql
 echo "Installing mysql repo packages"
-sudo yum install -y https://repo.mysql.com//mysql80-community-release-el7-5.noarch.rpm
+yum install -y https://repo.mysql.com//mysql80-community-release-el7-5.noarch.rpm
 wait
-sudo yum install -y mysql-server
+yum install -y mysql-server
 
 ## Starting mysql server (first run)'
-sudo systemctl start mysqld
+systemctl start mysqld
 
 ## Getting mysql temp root password
-tempRootDBPass="`sudo grep 'temporary.*root@localhost' /var/log/mysqld.log | tail -n 1 | sed 's/.*root@localhost: //'`"
+tempRootDBPass="`grep 'temporary.*root@localhost' /var/log/mysqld.log | tail -n 1 | sed 's/.*root@localhost: //'`"
 echo "myslq temp password is $tempRootDBPass"
 
 ## Setting up new mysql server root password'
-sudo systemctl stop mysqld.service
-sudo rm -rf /var/lib/mysql/*logfile*
-sudo systemctl start mysqld.service
+systemctl stop mysqld.service
+rm -rf /var/lib/mysql/*logfile*
+systemctl start mysqld.service
 mysqladmin -u root --password="$tempRootDBPass" password "$MYSQLROOTPASS"
 mysql -u root --password="$MYSQLROOTPASS" -e <<-EOSQL
     DELETE FROM mysql.user WHERE User='';
@@ -25,13 +25,13 @@ mysql -u root --password="$MYSQLROOTPASS" -e <<-EOSQL
     DELETE FROM mysql.user where user != 'mysql.sys';
     FLUSH PRIVILEGES;
 EOSQL
-sudo systemctl status mysqld.service
+systemctl status mysqld.service
 
 ## Creating database, user and password
 mysql -uroot -p$MYSQLROOTPASS <<QUERY_INPUT
-CREATE USER '$DBUSER'@'%' IDENTIFIED BY '$DBPASS';
-CREATE DATABASE $DBNAME;
-GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'%';
+CREATE USER '$ENVIRONMENT_NAME'@'%' IDENTIFIED BY '$DBPASS';
+CREATE DATABASE ${ENVIROMENT_NAME}-db;
+GRANT ALL PRIVILEGES ON ${ENVIRONMENT_NAME}-db.* TO '$ENVIRONMENT_NAME'@'%';
 FLUSH PRIVILEGES;
 EXIT
 QUERY_INPUT
